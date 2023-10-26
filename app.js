2722df7fbe513d03dcb4665ce113cb2e355731ec
unknown <a9500484054@yandex.ru>
2023-10-26 18:08:05 +0300

    first commit

--- app.js
+++ app.js
@@ -1,70 +1,68 @@
 class TreeStore {
-    items;
-    
+
     constructor(items) {
-      this.items = {};
-      items.forEach((item) => {
-        const { id, parent, ...rest } = item;
-        this.items[id] = { id, parent, ...rest };
-      });
+        this.items = {};
+        items.forEach((item) => {
+            const { id, parent, ...rest } = item;
+            this.items[id] = { id, parent, ...rest };
+        });
     }
-    
+
     getAll() {
-      return Object.values(this.items);
+        return Object.values(this.items);
     }
-    
+
     getItem(id) {
-      return this.items[id];
+        return this.items[id];
     }
-    
+
     getChildren(id) {
-      return Object.values(this.items).filter((item) => item.parent === id);
+        return Object.values(this.items).filter((item) => item.parent === id);
     }
-    
+
     getAllChildren(id) {
-      let children = [];
-      const stack = [this.items[id]];
-      
-      while (stack.length > 0) {
+        let children = [];
+        const stack = [this.items[id]];
+        
+        while (stack.length > 0) {
         const item = stack.pop();
         children.push(item);
         
         const itemChildren = Object.values(this.items).filter((child) => child.parent === item.id);
         
         stack.push(...itemChildren);
-      }
-      
-      return children;
+        }
+        
+        return children;
     }
-    
+
     getAllParents(id) {
-      const parents = [];
-      let currentId = id;
-      
-      while (this.items[currentId]) {
+        const parents = [];
+        let currentId = id;
+        
+        while (this.items[currentId]) {
         const currentItem = this.items[currentId];
         parents.unshift(currentItem);
         currentId = currentItem.parent;
-      }
-      
-      return parents;
+        }
+        
+        return parents;
     }
-  }
-  
-  // Example of usage:
-  const items = [
+}
+
+const items = [
     { id: 1, parent: 'root' },
     { id: 2, parent: 1, type: 'test' },
     { id: 3, parent: 1, type: 'test' },
     { id: 4, parent: 2, type: 'test' },
     { id: 5, parent: 2, type: 'test' },
     { id: 6, parent: 4, type: 'test' },
-  ];
-  
-  const store = new TreeStore(items);
-  
-  console.log(store.getAll()); // [{ id: 1, parent: 'root' }, { id: 2, parent: 1, type: 'test' }, ...]
-  console.log(store.getItem(3)); // { id: 3, parent: 1, type: 'test' }
-  console.log(store.getChildren(2)); // [{ id: 4, parent: 2, type: 'test' }, { id: 5, parent: 2, type: 'test' }]
-  console.log(store.getAllChildren(2)); // [{ id: 4, parent: 2, type: 'test' }, { id: 5, parent: 2, type: 'test' }, ...]
-  console.log(store.getAllParents(4)); // [{ id: 1, parent: 'root' }, { id: 2, parent: 1, type: 'test' }]
+];
+
+const store = new TreeStore(items);
+
+console.log(store.getAll()); 
+console.log(store.getItem(3)); 
+console.log(store.getChildren(2)); 
+console.log(store.getAllChildren(2)); 
+console.log(store.getAllParents(4)); 
